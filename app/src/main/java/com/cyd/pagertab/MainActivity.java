8e1a025ea5485ebf1d4a5e9be902494b08d45436
Villiage <changyadongholo@163.com>
2015-05-12 16:14:18 +0800

    first commit

---
+++ app/src/main/java/com/cyd/pagertab/MainActivity.java
@@ -0,0 +1,92 @@
+package com.cyd.pagertab;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+
+    int[] colors = {android.R.color.holo_blue_light, android.R.color.holo_orange_light, android.R.color.holo_red_light, android.R.color.holo_green_light};
+    List<View> views = new ArrayList<View>();
+    TabLayout container;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        container = (TabLayout)findViewById(R.id.container);
+        getViews();
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+       pager.setOnPageChangeListener(opcl);
+
+    }
+    PagerAdapter adapter = new PagerAdapter() {
+
+        @Override
+        public int getCount() {
+            return colors.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            container.addView(views.get(position));
+
+            return views.get(position);
+
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+    };
+    ViewPager.OnPageChangeListener opcl = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            container.setProgress(position, positionOffset);
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            container.setPosition(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+    public void getViews() {
+        for (int i = 0; i < colors.length; i++) {
+            View v = new View(this);
+
+            v.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            v.setBackgroundColor(getResources().getColor(colors[i]));
+            views.add(v);
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/cyd/pagertab/TabLayout.java
@@ -0,0 +1,111 @@
+package com.cyd.pagertab;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by dong on 2015/5/8.
+ */
+public class TabLayout extends LinearLayout {
+    int position = 0;
+    int childCount = 4;
+    int smallRectW = 20;
+    int strokeWidth = 1;
+    float progress;
+
+    public TabLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    public TabLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public TabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+
+    public void init() {
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int parentWidth = canvas.getWidth();
+        int width = parentWidth / childCount;
+        int height = canvas.getHeight();
+//        ±ß¿ò
+//        int radius = smallRectW / 2;
+//        int left = (int) (width * (position + progress));
+
+
+//        Paint paint = new Paint();
+//        paint.setAntiAlias(true);
+//        paint.setColor(Color.BLACK);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setStrokeWidth(strokeWidth);
+
+
+//        canvas.drawLine(left, radius, left, height, paint);
+//        canvas.drawLine(left + radius, 0, left + width - radius, 0, paint);
+//        canvas.drawLine(left + width, radius, left + width, height, paint);
+//
+//        RectF rectF = new RectF();
+//        rectF.set(left, 0, left + smallRectW, smallRectW);
+//        canvas.drawArc(rectF, 180, 90, false, paint);
+//
+//        rectF.set(left + width - smallRectW, 0, left + width, smallRectW);
+//        canvas.drawArc(rectF, 270, 90, false, paint);
+//
+//        canvas.drawLine(0, height - strokeWidth, left, height - strokeWidth, paint);
+//        canvas.drawLine(left + width, height - strokeWidth, parentWidth, height - strokeWidth, paint);
+
+//        Í¸Ã÷¶È½¥±ä±³¾°
+//        Paint paint = new Paint();
+//        paint.setAntiAlias(true);
+//        paint.setColor(Color.CYAN);
+//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+//
+//
+//        int left1 = position * width;
+//        Rect rect1 = new Rect();
+//        rect1.set(left1, 0, left1 + width, height);
+//        paint.setAlpha((int) (255 * (1 - progress)));
+//        canvas.drawRect(rect1, paint);
+//
+//        int left2 = (position + 1) * width;
+//        Rect rect2 = new Rect();
+//        rect2.set(left2, 0, left2 + width, height);
+//        paint.setAlpha((int) (225 * progress));
+//        canvas.drawRect(rect2, paint);
+
+
+    }
+
+
+    public void setPosition(int position) {
+        this.position = position;
+        invalidate();
+    }
+
+    public void setProgress(int position, float progress) {
+        this.position = position;
+        this.progress = progress;
+        invalidate();
+
+    }
+
+}
